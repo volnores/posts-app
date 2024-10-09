6ebf039ed29b72de83a3086024d579b5261c7134
volnores <volnores2409@mail.ru>
2024-10-09 20:35:30 +0300

    add pagination

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,24 +4,47 @@ import NewsPost from "../../components/NewsPost/NewsPost";
 import { getPosts } from "../../api/apiNews";
 import NewsList from "../../components/NewsList/NewsList";
 import Skeleton from "../../components/Skeleton/Skeleton";
+import Pagination from "../../components/Pagination/Pagination";
 
 const Main = () => {
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  useEffect(() => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 10;
+  const totalPages = 10;
+
+  const fetchPosts = async (currentPage) => {
     try {
       setIsLoading(true);
-      const fetchPosts = async () => {
-        const response = await getPosts();
-        setPosts(response.news);
-      };
-      fetchPosts();
+      const response = await getPosts(currentPage, pageSize);
+      setPosts(response.news);
+      setIsLoading(false);
     } catch (error) {
       alert(error);
     } finally {
       setIsLoading(false);
     }
-  }, []);
+  };
+
+  const handleNextPage = () => {
+    {
+      currentPage < totalPages ? setCurrentPage(currentPage + 1) : currentPage;
+    }
+  };
+
+  const handlePrevPage = () => {
+    {
+      currentPage > 1 ? setCurrentPage(currentPage - 1) : currentPage;
+    }
+  };
+
+  const handleCurrentPage = (page) => {
+    setCurrentPage(page);
+  };
+
+  useEffect(() => {
+    fetchPosts(currentPage);
+  }, [currentPage]);
   return (
     <main className={style.main}>
       {posts.length > 0 && !isLoading ? (
@@ -29,6 +52,14 @@ const Main = () => {
       ) : (
         <Skeleton type={"post"} count={1} />
       )}
+
+      <Pagination
+        totalPages={totalPages}
+        handleNextPage={handleNextPage}
+        handlePrevPage={handlePrevPage}
+        handleCurrentPage={handleCurrentPage}
+        currentPage={currentPage}
+      />
 
       {!isLoading ? (
         <NewsList posts={posts} />
